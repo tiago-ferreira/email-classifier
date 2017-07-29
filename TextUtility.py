f1be0bfb6d5fcfec294b241012b8e743a6784740
tiago <si.tiagoferreira@gmail.com>
2017-07-29 13:20:59 -0300

    utility methods and gitignore

--- TextUtility.py
+++ TextUtility.py
@@ -1,8 +1,24 @@
 import nltk
 
 class TextUtility(object):
-  def brokenText(self, data) :
+
+
+  def broken_text(self, data) :
     texts = data.str.lower()
     broken_text = [nltk.tokenize.word_tokenize(text) for text in texts]
     return broken_text
 
+  def build_dictionary(self, data):
+    stopwords = nltk.corpus.stopwords('portuguese')
+    stemmer = nltk.stem.RSLPStemmer()
+    dictionary = set()
+    for value in data:
+      validWords = [stemmer.stem(word) for word in value if word not in stopwords and len(word) > 2]
+      dictionary.update(validWords)
+    return dictionary
+
+  def build_map_words_position(self, data):
+    totalOfWords = len(data)
+    tupleWords = zip(data, xrange(totalOfWords))
+    mapWordsAndPositions = {word:index for word,index in tupleWords}
+    return mapWordsAndPositions
